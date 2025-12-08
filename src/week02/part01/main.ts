28ed77897fe6f763ddb3e3065e8f791390091f1f
Mads Rumle Nordstrøm <madsrumlenordstrom@icloud.com>
2025-12-08 21:12:22 +0100

    init week 2 part 01

---
+++ src/week02/part01/main.ts
@@ -0,0 +1,18 @@
+"use strict";
+window.onload = function() { main(); }
+
+import shader from "./shader.wgsl?raw";
+
+const main= async() =>
+{
+  const adapter : GPUAdapter = <GPUAdapter> await navigator.gpu.requestAdapter();
+  const device : GPUDevice = await adapter.requestDevice();
+  const canvas : HTMLCanvasElement = <HTMLCanvasElement> document.getElementById('gpu-canvas');
+  const context : GPUCanvasContext = <GPUCanvasContext> canvas.getContext('webgpu');
+  const format : GPUTextureFormat = navigator.gpu.getPreferredCanvasFormat();
+  const configuration : GPUCanvasConfiguration = {
+    device: device,
+    format: format,
+  };
+  context.configure(configuration);
+}
